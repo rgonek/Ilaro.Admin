59ab3ac17d87f1ea03a4d31b3d6e9356d3b88a56
Robert Gonek <rgonek@gmail.com>
2014-04-09 21:01:46 +0200

    Delete unnecessary files
    I hope now repo will be language setted to C#

--- src/Ilaro.Admin/Ilaro.Admin/Views/IlaroAdmin/_Layout.generated.cs
+++ src/Ilaro.Admin/Ilaro.Admin/Views/IlaroAdmin/_Layout.generated.cs
@@ -67,7 +67,7 @@ WriteLiteral(" content=\"width=device-width, initial-scale=1.0\"");
 
 WriteLiteral(">\r\n    <link");
 
-WriteLiteral(" href=\"/IlaroAdminResource/css/bootstrap_css\"");
+WriteLiteral(" href=\"/ira/css/bootstrap_css\"");
 
 WriteLiteral(" rel=\"stylesheet\"");
 
@@ -80,7 +80,7 @@ WriteLiteral(" type=\"text/css\"");
 WriteLiteral(">\r\n        body {\r\n            padding-top: 60px;\r\n            padding-bottom: 40" +
 "px;\r\n        }\r\n    </style>\r\n    <link");
 
-WriteLiteral(" href=\"/IlaroAdminResource/css/google-bootstrap_css\"");
+WriteLiteral(" href=\"/ira/css/google-bootstrap_css\"");
 
 WriteLiteral(" rel=\"stylesheet\"");
 
@@ -88,7 +88,7 @@ WriteLiteral(" type=\"text/css\"");
 
 WriteLiteral(" />\r\n    <link");
 
-WriteLiteral(" href=\"/IlaroAdminResource/css/bootstrap-responsive_css\"");
+WriteLiteral(" href=\"/ira/css/bootstrap-responsive_css\"");
 
 WriteLiteral(" rel=\"stylesheet\"");
 
@@ -96,7 +96,7 @@ WriteLiteral(" type=\"text/css\"");
 
 WriteLiteral(" />\r\n    <link");
 
-WriteLiteral(" href=\"/IlaroAdminResource/css/bootstrap-datepicker_css\"");
+WriteLiteral(" href=\"/ira/css/bootstrap-datepicker_css\"");
 
 WriteLiteral(" rel=\"stylesheet\"");
 
@@ -104,7 +104,7 @@ WriteLiteral(" type=\"text/css\"");
 
 WriteLiteral(" />\r\n    <link");
 
-WriteLiteral(" href=\"/IlaroAdminResource/css/google-datepicker_css\"");
+WriteLiteral(" href=\"/ira/css/google-datepicker_css\"");
 
 WriteLiteral(" rel=\"stylesheet\"");
 
@@ -112,7 +112,7 @@ WriteLiteral(" type=\"text/css\"");
 
 WriteLiteral(" />\r\n    <link");
 
-WriteLiteral(" href=\"/IlaroAdminResource/css/font-awesome_css\"");
+WriteLiteral(" href=\"/ira/css/font-awesome_css\"");
 
 WriteLiteral(" rel=\"stylesheet\"");
 
@@ -120,7 +120,7 @@ WriteLiteral(" type=\"text/css\"");
 
 WriteLiteral(" />\r\n    <link");
 
-WriteLiteral(" href=\"/IlaroAdminResource/css/font-awesome-ie7_min_css\"");
+WriteLiteral(" href=\"/ira/css/font-awesome-ie7_min_css\"");
 
 WriteLiteral(" rel=\"stylesheet\"");
 
@@ -128,7 +128,7 @@ WriteLiteral(" type=\"text/css\"");
 
 WriteLiteral(" />\r\n    <link");
 
-WriteLiteral(" href=\"/IlaroAdminResource/css/select2_css\"");
+WriteLiteral(" href=\"/ira/css/select2_css\"");
 
 WriteLiteral(" rel=\"stylesheet\"");
 
@@ -136,7 +136,7 @@ WriteLiteral(" type=\"text/css\"");
 
 WriteLiteral(" />\r\n    <link");
 
-WriteLiteral(" href=\"/IlaroAdminResource/css/bootstrap-wysihtml5_css\"");
+WriteLiteral(" href=\"/ira/css/bootstrap-wysihtml5_css\"");
 
 WriteLiteral(" rel=\"stylesheet\"");
 
@@ -144,15 +144,7 @@ WriteLiteral(" type=\"text/css\"");
 
 WriteLiteral(" />\r\n    <link");
 
-WriteLiteral(" href=\"/IlaroAdminResource/css/bootstrap-jasny_css\"");
-
-WriteLiteral(" rel=\"stylesheet\"");
-
-WriteLiteral(" type=\"text/css\"");
-
-WriteLiteral(" />\r\n    <link");
-
-WriteLiteral(" href=\"/IlaroAdminResource/css/Site_css\"");
+WriteLiteral(" href=\"/ira/css/Site_css\"");
 
 WriteLiteral(" rel=\"stylesheet\"");
 
@@ -164,35 +156,12 @@ WriteLiteral(" src=\"/IlaroAdminResource/Script/jquery-1_9_0_min_js\"");
 
 WriteLiteral(" type=\"text/javascript\"");
 
-WriteLiteral("></script>\r\n    <script");
-
-WriteLiteral(" src=\"/IlaroAdminResource/Script/jquery_mb_browser_js\"");
-
-WriteLiteral(" type=\"text/javascript\"");
-
-WriteLiteral("></script>\r\n    <script");
-
-WriteLiteral(" src=\"/IlaroAdminResource/Script/modernizr-2_6_2_js\"");
-
-WriteLiteral(" type=\"text/javascript\"");
-
 WriteLiteral("></script>\r\n");
 
 WriteLiteral("    ");
 
             
-            #line 31 "..\..\Views\IlaroAdmin\_Layout.cshtml"
-Write(Html.Partial("_html5shiv"));
-
-            
-            #line default
-            #line hidden
-WriteLiteral("\r\n");
-
-WriteLiteral("    ");
-
-            
-            #line 32 "..\..\Views\IlaroAdmin\_Layout.cshtml"
+            #line 28 "..\..\Views\IlaroAdmin\_Layout.cshtml"
 Write(RenderSection("Header", false));
 
             
@@ -215,7 +184,7 @@ WriteLiteral(">\r\n");
 WriteLiteral("                ");
 
             
-            #line 38 "..\..\Views\IlaroAdmin\_Layout.cshtml"
+            #line 34 "..\..\Views\IlaroAdmin\_Layout.cshtml"
            Write(Html.ActionLink("Admin", "Index", "IlaroAdmin", null, new { @class = "brand" }));
 
             
@@ -247,7 +216,7 @@ WriteLiteral(">\r\n");
 WriteLiteral("                ");
 
             
-            #line 50 "..\..\Views\IlaroAdmin\_Layout.cshtml"
+            #line 46 "..\..\Views\IlaroAdmin\_Layout.cshtml"
            Write(RenderSection("Breadcrumb", false));
 
             
@@ -256,13 +225,13 @@ WriteLiteral("                ");
 WriteLiteral("\r\n            </div>\r\n");
 
             
-            #line 52 "..\..\Views\IlaroAdmin\_Layout.cshtml"
+            #line 48 "..\..\Views\IlaroAdmin\_Layout.cshtml"
             
             
             #line default
             #line hidden
             
-            #line 52 "..\..\Views\IlaroAdmin\_Layout.cshtml"
+            #line 48 "..\..\Views\IlaroAdmin\_Layout.cshtml"
               Html.RenderAction("Messages");
             
             #line default
@@ -276,7 +245,7 @@ WriteLiteral(">\r\n");
 WriteLiteral("                ");
 
             
-            #line 54 "..\..\Views\IlaroAdmin\_Layout.cshtml"
+            #line 50 "..\..\Views\IlaroAdmin\_Layout.cshtml"
            Write(RenderBody());
 
             
@@ -291,7 +260,7 @@ WriteLiteral(">\r\n");
 WriteLiteral("                ");
 
             
-            #line 57 "..\..\Views\IlaroAdmin\_Layout.cshtml"
+            #line 53 "..\..\Views\IlaroAdmin\_Layout.cshtml"
            Write(RenderSection("Sidebar", false));
 
             
@@ -301,7 +270,7 @@ WriteLiteral("\r\n            </div>\r\n        </div>\r\n        <hr>\r\n      
 " <p>&copy; Ilaro.Admin ");
 
             
-            #line 62 "..\..\Views\IlaroAdmin\_Layout.cshtml"
+            #line 58 "..\..\Views\IlaroAdmin\_Layout.cshtml"
                              Write(System.DateTime.Now.ToString("yyyy"));
 
             
@@ -323,45 +292,45 @@ WriteLiteral(" id=\"modal-image\"");
 
 WriteLiteral(" />\r\n        </div>\r\n    </div>\r\n\r\n    <script");
 
-WriteLiteral(" src=\"/IlaroAdminResource/Script/jquery_validate_min_js\"");
+WriteLiteral(" src=\"/ira/Script/jquery_validate_min_js\"");
 
 WriteLiteral(" type=\"text/javascript\"");
 
 WriteLiteral("></script>\r\n    <script");
 
-WriteLiteral(" src=\"/IlaroAdminResource/Script/jquery_validate_unobtrusive_min_js\"");
+WriteLiteral(" src=\"/ira/Script/jquery_validate_unobtrusive_min_js\"");
 
 WriteLiteral(" type=\"text/javascript\"");
 
 WriteLiteral("></script>\r\n    <script");
 
-WriteLiteral(" src=\"/IlaroAdminResource/Script/select2_js\"");
+WriteLiteral(" src=\"/ira/Script/jquery_validate_unobtrusive-custom-for-bootstrap_js\"");
+
+WriteLiteral("></script>\r\n    <script");
+
+WriteLiteral(" src=\"/ira/Script/select2_js\"");
 
 WriteLiteral(" type=\"text/javascript\"");
 
 WriteLiteral("></script>\r\n    <script");
 
-WriteLiteral(" src=\"/IlaroAdminResource/Script/bootstrap-datepicker_js\"");
+WriteLiteral(" src=\"/ira/Script/bootstrap-datepicker_js\"");
 
 WriteLiteral(" type=\"text/javascript\"");
 
 WriteLiteral("></script>\r\n    <script");
 
-WriteLiteral(" src=\"/IlaroAdminResource/Script/bootstrap_min_js\"");
+WriteLiteral(" src=\"/ira/Script/bootstrap_min_js\"");
 
 WriteLiteral(" type=\"text/javascript\"");
 
 WriteLiteral("></script>\r\n    <script");
 
-WriteLiteral(" src=\"/IlaroAdminResource/Script/wysihtml5-0_3_0_js\"");
+WriteLiteral(" src=\"/ira/Script/wysihtml5-0_3_0_js\"");
 
 WriteLiteral("></script>\r\n    <script");
 
-WriteLiteral(" src=\"/IlaroAdminResource/Script/bootstrap-wysihtml5_js\"");
-
-WriteLiteral("></script>\r\n    <script");
-
-WriteLiteral(" src=\"/IlaroAdminResource/Script/bootstrap-jasny_js\"");
+WriteLiteral(" src=\"/ira/Script/bootstrap-wysihtml5_js\"");
 
 WriteLiteral("></script>\r\n    <script");
 
@@ -374,8 +343,6 @@ WriteLiteral(@">
             $('textarea.html').each(function () {
                 $(this).wysihtml5({ html: true, color: true, stylesheets: false }); 
             });
-            $('.inputmask').inputmask();
-            $('.fileupload').fileupload();
             $('th > span').tooltip();
             $('.open-modal').click(function (e) {
                 e.preventDefault();
@@ -407,7 +374,7 @@ WriteLiteral(@">
 WriteLiteral("    ");
 
             
-            #line 115 "..\..\Views\IlaroAdmin\_Layout.cshtml"
+            #line 109 "..\..\Views\IlaroAdmin\_Layout.cshtml"
 Write(RenderSection("Scripts", false));
 
             
