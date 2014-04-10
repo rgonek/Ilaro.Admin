5d96889d960cea3df9d5e8c617df618ad98d68c4
Robert Gonek <rgonek@gmail.com>
2014-04-10 21:42:23 +0200

    Publish settings
    Add publish settings and server db

--- src/Ilaro.Admin/Ilaro.Admin.Sample/Global.asax.cs
+++ src/Ilaro.Admin/Ilaro.Admin.Sample/Global.asax.cs
@@ -11,9 +11,6 @@ using System.Web.Routing;
 
 namespace Ilaro.Admin.Sample
 {
-    // Note: For instructions on enabling IIS6 or IIS7 classic mode, 
-    // visit http://go.microsoft.com/?LinkId=9394801
-
     public class MvcApplication : System.Web.HttpApplication
     {
         protected void Application_Start()
