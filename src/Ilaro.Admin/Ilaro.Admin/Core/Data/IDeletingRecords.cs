bf8e7920dbb63d772e3716f19568bfc2a621f5c8
Robert Gonek <rgonek@gmail.com>
2015-07-05 11:52:08 +0200

    Records deleter

---
+++ src/Ilaro.Admin/Ilaro.Admin/Core/Data/IDeletingRecords.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Generic;
+using Ilaro.Admin.Models;
+
+namespace Ilaro.Admin.Core.Data
+{
+    public interface IDeletingRecords
+    {
+        bool Delete(Entity entity, IDictionary<string, DeleteOption> options);
+    }
+}
