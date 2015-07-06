84adaccd1e0ed7f73d85004d5ff0528a3fa03fb9
Robert Gonek <rgonek@gmail.com>
2015-07-06 22:42:16 +0200

    Added IKnowTheTime

---
+++ src/Ilaro.Admin/Ilaro.Admin.Tests/FakeClock.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Ilaro.Admin.Core;
+
+namespace Ilaro.Admin.Tests
+{
+    public class FakeClock : IKnowTheTime
+    {
+        private readonly Func<DateTime> _utcNow;
+        private readonly Func<DateTime> _now;
+
+        public DateTime UtcNow
+        {
+            get { return _utcNow(); }
+        }
+        public DateTime Now
+        {
+            get { return _now(); }
+        }
+
+        public FakeClock(Func<DateTime> utcNow, Func<DateTime> now)
+        {
+            if (utcNow == null) throw new ArgumentNullException("utcNow");
+            if (now == null) throw new ArgumentNullException("now");
+
+            _utcNow = utcNow;
+            _now = now;
+        }
+    }
+}
