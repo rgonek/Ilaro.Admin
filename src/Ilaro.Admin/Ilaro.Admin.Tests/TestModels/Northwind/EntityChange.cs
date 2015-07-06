41ee377a3d9ee81c5f42b4b4d6776e991c60c778
Robert Gonek <rgonek@gmail.com>
2015-07-06 21:51:34 +0200

    Test models (same as in sample)

---
+++ src/Ilaro.Admin/Ilaro.Admin.Tests/TestModels/Northwind/EntityChange.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Ilaro.Admin.Core;
+
+namespace Ilaro.Admin.Tests.TestModels.Northwind
+{
+	public class EntityChange : IEntityChange
+	{
+		public int EntityChangeId { get; set; }
+
+		public string EntityName { get; set; }
+
+		public string EntityKey { get; set; }
+
+		public EntityChangeType ChangeType { get; set; }
+
+		public string Description { get; set; }
+
+		public DateTime ChangedOn { get; set; }
+
+		public string ChangedBy { get; set; }
+	}
+}
