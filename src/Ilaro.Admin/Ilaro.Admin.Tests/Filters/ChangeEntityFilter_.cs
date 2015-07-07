1722543dc7b0e98c062bbe88a4756f9a4b31a009
Robert Gonek <rgonek@gmail.com>
2015-07-07 17:57:00 +0200

    Tests for ChangeEntityFilter

---
+++ src/Ilaro.Admin/Ilaro.Admin.Tests/Filters/ChangeEntityFilter_.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections.Generic;
+using Ilaro.Admin.Core;
+using Ilaro.Admin.Filters;
+using Xunit;
+
+namespace Ilaro.Admin.Tests.Filters
+{
+    public class ChangeEntityFilter_
+    {
+        private ChangeEntityFilter _filter;
+        private Property _property;
+
+        public ChangeEntityFilter_()
+        {
+            var entity = new Entity(typeof(TestEntity));
+            _property = entity["Name"];
+
+            _filter = new ChangeEntityFilter(_property, "entity_name");
+        }
+
+        [Fact]
+        public void generated_options_should_match()
+        {
+            var options = _filter.Options;
+            Assert.Null(_filter.Options);
+        }
+
+        [Fact]
+        public void sql_condition_should_match()
+        {
+            var args = new List<object>();
+            var sql = _filter.GetSqlCondition("t0", ref args);
+
+            Assert.Equal("t0[Name] = @0", sql);
+        }
+
+        [Fact]
+        public void arguments_should_match()
+        {
+            var args = new List<object>();
+            _filter.GetSqlCondition("t0", ref args);
+
+            Assert.Equal(1, args.Count);
+            Assert.Equal("entity_name", args[0]);
+        }
+    }
+}
