03082c04ab8df4dd78bc331bc0406c66d774c5e0
Robert Gonek <rgonek@gmail.com>
2015-07-08 18:50:45 +0200

    RecordsSource ForeignEntityFilters tests

---
+++ src/Ilaro.Admin/Ilaro.Admin.Tests/Core/Data/RecordsSource_ForeignEntityFilters.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Ilaro.Admin.Core;
+using Ilaro.Admin.Core.Data;
+using Ilaro.Admin.Filters;
+using Ilaro.Admin.Tests.TestModels.Northwind;
+using Xunit;
+
+namespace Ilaro.Admin.Tests.Core.Data
+{
+    public class RecordsSource_ForeignEntityFilters : SqlServerDatabaseTest
+    {
+        private readonly IFetchingRecords _source;
+        private Entity _entity;
+        private Property _property;
+        private int _supplierId;
+
+        public RecordsSource_ForeignEntityFilters()
+        {
+            _supplierId = DB.Suppliers.Insert(CompanyName: "Supplier").SupplierID;
+            DB.Products.Insert(ProductName: "Product", SupplierID: _supplierId);
+            DB.Products.Insert(ProductName: "Product2");
+
+            _source = new RecordsSource(new Notificator());
+            AdminInitialise.AddEntity<Product>();
+            AdminInitialise.SetForeignKeysReferences();
+            AdminInitialise.ConnectionStringName = ConnectionStringName;
+            _entity = AdminInitialise.EntitiesTypes
+                .FirstOrDefault(x => x.Name == "Product");
+            _property = _entity["SupplierID"];
+        }
+
+        [Fact]
+        public void empty_foreign_filter_should_return_two_records()
+        {
+            var result = _source.GetRecords(_entity);
+            Assert.Equal(2, result.Records.Count);
+
+            var filters = new List<IEntityFilter>();
+            result = _source.GetRecords(_entity, filters);
+            Assert.Equal(2, result.Records.Count);
+
+            filters = new List<IEntityFilter>
+            {
+                new ForeignEntityFilter(_property)
+            };
+            result = _source.GetRecords(_entity, filters);
+            Assert.Equal(2, result.Records.Count);
+        }
+
+        [Fact]
+        public void foreign_filter_with_value_should_return_one_records()
+        {
+            var filters = new List<IEntityFilter>
+            {
+                new ForeignEntityFilter(_property, _supplierId.ToString())
+            };
+            var result = _source.GetRecords(_entity, filters);
+            Assert.Equal(1, result.Records.Count);
+            Assert.Equal("Product", result.Records[0].Values[1].AsString);
+        }
+    }
+}
