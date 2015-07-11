ad940dd18b02cc6e66623aab3ff6217bab73d941
Robert Gonek <rgonek@gmail.com>
2015-07-11 10:10:32 +0200

    Copy IsAjax rewquest attribute

---
+++ src/Ilaro.Admin/Ilaro.Admin/Infrastructure/CopyIsAjaxRequestFromRequestToViewBagAttribute.cs
@@ -0,0 +1,17 @@
+﻿using System.Web.Mvc;
+
+namespace Ilaro.Admin.Infrastructure
+{
+    public class CopyIsAjaxRequestFromRequestToViewBagAttribute : ActionFilterAttribute
+    {
+        public override void OnActionExecuted(ActionExecutedContext filterContext)
+        {
+            var view = filterContext.Result as ViewResult;
+            if (view != null)
+            {
+                view.ViewBag.IsAjaxRequest =
+                    filterContext.RequestContext.HttpContext.Request.IsAjaxRequest();
+            }
+        }
+    }
+}
