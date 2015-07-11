bd104ae660b2de0ba75b4dec63354db7d128df56
Robert Gonek <rgonek@gmail.com>
2015-07-11 10:09:58 +0200

    NotificatorAttribute, notification between requests

---
+++ src/Ilaro.Admin/Ilaro.Admin/Infrastructure/NotificatorAttribute.cs
@@ -0,0 +1,47 @@
+﻿using System.Web.Mvc;
+using Ilaro.Admin.Core;
+
+namespace Ilaro.Admin.Infrastructure
+{
+    public class NotificatorAttribute : ActionFilterAttribute
+    {
+        public override void OnActionExecuting(ActionExecutingContext filterContext)
+        {
+            var view = filterContext.Result as ViewResult;
+            if (view == null)
+            {
+                return;
+            }
+
+            var notificator =
+                (Notificator)DependencyResolver.Current.GetService(typeof(Notificator));
+
+            var notificator2 = (Notificator)view.TempData["Notificator"];
+
+            foreach (var kvp in notificator2.Messages)
+            {
+                if (kvp.Value.Count > 0)
+                {
+                    while (kvp.Value.Count > 0)
+                    {
+                        notificator.Notificate(kvp.Value.Dequeue(), kvp.Key);
+                    }
+                }
+            }
+        }
+
+        public override void OnActionExecuted(ActionExecutedContext filterContext)
+        {
+            var view = filterContext.Result as ViewResult;
+            if (view == null)
+            {
+                return;
+            }
+
+            var notificator =
+                (Notificator)DependencyResolver.Current.GetService(typeof(Notificator));
+
+            view.TempData["Notificator"] = notificator;
+        }
+    }
+}
