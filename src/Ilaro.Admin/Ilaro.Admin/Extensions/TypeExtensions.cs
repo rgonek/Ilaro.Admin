a72d7d94e21b3b6abe8fb3291fafadd47ab7d1bd
Robert Gonek <rgonek@gmail.com>
2016-03-06 11:11:15 +0100

    Added scanning assemblies and registering all matching types

---
+++ src/Ilaro.Admin/Ilaro.Admin/Extensions/TypeExtensions.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Reflection;
+
+namespace Ilaro.Admin.Extensions
+{
+    static class TypeExtensions
+    {
+        public static bool IsDelegate(this Type type)
+        {
+            if (type == null) throw new ArgumentNullException(nameof(type));
+
+            return type.GetTypeInfo().IsSubclassOf(typeof(Delegate));
+        }
+    }
+}
---
+++ src/Ilaro.Admin/Ilaro.Admin/Registration/ScanningRegistrationExtensions.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using Ilaro.Admin.Extensions;
+
+namespace Ilaro.Admin.Registration
+{
+    static class ScanningRegistrationExtensions
+    {
+        public static RegistrationBuilder
+               RegisterAssemblyTypes(params Assembly[] assemblies)
+        {
+            if (assemblies == null) throw new ArgumentNullException(nameof(assemblies));
+
+            var rb = new RegistrationBuilder();
+
+            rb.RegisterCallback(() => ScanAssemblies(assemblies, rb));
+
+            return rb;
+        }
+
+        public static RegistrationBuilder
+               RegisterTypes(params Type[] types)
+        {
+            if (types == null) throw new ArgumentNullException(nameof(types));
+
+            var rb = new RegistrationBuilder();
+
+            rb.RegisterCallback(() => ScanTypes(types, rb));
+
+            return rb;
+        }
+
+        static void ScanAssemblies(IEnumerable<Assembly> assemblies, RegistrationBuilder rb)
+        {
+            ScanTypes(assemblies.SelectMany(a => a.GetLoadableTypes()), rb);
+        }
+
+        static void ScanTypes(IEnumerable<Type> types, RegistrationBuilder rb)
+        {
+            foreach (var t in types
+                .Where(t => t.GetTypeInfo().IsClass &&
+                !t.GetTypeInfo().IsAbstract &&
+                !t.GetTypeInfo().IsGenericTypeDefinition &&
+                !t.IsDelegate() &&
+                rb.Filters.All(p => p(t))))
+            {
+                Admin.RegisterEntity(t);
+            }
+        }
+    }
+}
