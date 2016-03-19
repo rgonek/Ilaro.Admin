6f6deb1a0bd8d12be3d31db0d10815b02e9cdb27
Robert Gonek <rgonek@gmail.com>
2016-03-19 16:37:10 +0100

    Added entity initialise test and TestBase

---
+++ src/Ilaro.Admin/Ilaro.Admin.Tests/TestBase.cs
@@ -0,0 +1,19 @@
+﻿using FakeItEasy;
+using System.Web.Mvc;
+
+namespace Ilaro.Admin.Tests
+{
+    public class TestBase
+    {
+        protected readonly IIlaroAdmin _admin;
+
+        public TestBase()
+        {
+            _admin = new IlaroAdmin();
+            var resolver = A.Fake<IDependencyResolver>();
+            DependencyResolver.SetResolver(resolver);
+            A.CallTo(() => resolver.GetService(typeof(IIlaroAdmin)))
+                .Returns(_admin);
+        }
+    }
+}
