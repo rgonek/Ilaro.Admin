c94aa3dc8a8cbcebd00ba64dc85dfa59d6a50b08
Robert Gonek <rgonek@gmail.com>
2016-03-30 22:15:25 +0200

    Removed unused usings

--- src/Ilaro.Admin/Ilaro.Admin/Core/Entity.cs
+++ src/Ilaro.Admin/Ilaro.Admin/Core/Entity.cs
@@ -1,12 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Diagnostics;
-using System.Globalization;
 using System.Linq;
-using System.Web;
-using System.Web.Mvc;
-using Ilaro.Admin.Core.Data;
-using Ilaro.Admin.DataAnnotations;
 using Ilaro.Admin.Extensions;
 using Ilaro.Admin.Models;
 
