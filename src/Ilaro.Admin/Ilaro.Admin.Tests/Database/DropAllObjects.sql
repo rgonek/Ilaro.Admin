68c0654dc0c73c4c038673f825710dee85fd68a5
Robert Gonek <rgonek@gmail.com>
2016-04-06 21:57:04 +0200

    DropAllObjects sql script is db name agnostic

--- src/Ilaro.Admin/Ilaro.Admin.Tests/Database/DropAllObjects.sql
+++ src/Ilaro.Admin/Ilaro.Admin.Tests/Database/DropAllObjects.sql
@@ -1,11 +1,7 @@
-﻿IF (exists (select * from sysdatabases where name='IlaroTestDb'))
-BEGIN
-    USE [IlaroTestDb]
-    -- disable referential integrity
-    EXEC sp_MSForEachTable 'ALTER TABLE ? NOCHECK CONSTRAINT ALL' 
+﻿-- disable referential integrity
+EXEC sp_MSForEachTable 'ALTER TABLE ? NOCHECK CONSTRAINT ALL' 
 
-    EXEC sp_MSForEachTable 'DELETE FROM ?' 
+EXEC sp_MSForEachTable 'DELETE FROM ?' 
 
-    -- enable referential integrity again 
-    EXEC sp_MSForEachTable 'ALTER TABLE ? WITH CHECK CHECK CONSTRAINT ALL' 
-END
+-- enable referential integrity again 
+EXEC sp_MSForEachTable 'ALTER TABLE ? WITH CHECK CHECK CONSTRAINT ALL' 
