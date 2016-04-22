8b8102aac6dbc6d85588a133427c0722e31645ce
Robert Gonek <rgonek@gmail.com>
2016-04-22 22:10:08 +0200

    Added configuration for entity change

---
+++ samples/Ilaro.Admin.Sample/Configurators/EntityChangeConfiguration.cs
@@ -0,0 +1,9 @@
+﻿using Ilaro.Admin.Configuration;
+using Ilaro.Admin.Sample.Models.Northwind;
+
+namespace Ilaro.Admin.Sample.Configurators
+{
+    public class EntityChangeConfiguration : EntityConfiguration<EntityChange>
+    {
+    }
+}
