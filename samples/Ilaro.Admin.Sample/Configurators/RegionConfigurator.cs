3d95d3449af5c6bc92ca70112d66111e2d19cf03
Robert Gonek <rgonek@gmail.com>
2016-04-22 21:42:08 +0200

    Added configuration for region

---
+++ samples/Ilaro.Admin.Sample/Configurators/RegionConfigurator.cs
@@ -0,0 +1,20 @@
+﻿using Ilaro.Admin.Configuration;
+using Ilaro.Admin.Sample.Models.Northwind;
+
+namespace Ilaro.Admin.Sample.Configurators
+{
+    public class RegionConfigurator : EntityConfiguration<Region>
+    {
+        public RegionConfigurator()
+        {
+            Table("Region");
+
+            Property(x => x.RegionDescription, x =>
+            {
+                x.Required();
+                x.StringLength(50);
+                x.Template(editor: Templates.Editor.TextArea);
+            });
+        }
+    }
+}
--- samples/Ilaro.Admin.Sample/Models/Northwind/Region.cs
+++ samples/Ilaro.Admin.Sample/Models/Northwind/Region.cs
@@ -1,4 +1,5 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using Ilaro.Admin.DataAnnotations;
+using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 
 namespace Ilaro.Admin.Sample.Models.Northwind
@@ -9,6 +10,7 @@ namespace Ilaro.Admin.Sample.Models.Northwind
         public int RegionID { get; set; }
 
         [Required, StringLength(50)]
+        [Template(EditorTemplate = Templates.Editor.TextArea)]
         public string RegionDescription { get; set; }
     }
 }
