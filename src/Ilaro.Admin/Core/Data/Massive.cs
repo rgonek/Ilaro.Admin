1cccfbe2f5aa1920518546c4b142c8be95005f06
Robert Gonek <rgonek@gmail.com>
2016-05-14 19:47:02 +0200

    Alias in Paged query is always equal to table name

--- src/Ilaro.Admin/Core/Data/Massive.cs
+++ src/Ilaro.Admin/Core/Data/Massive.cs
@@ -199,9 +199,9 @@ namespace Massive
 
             var query = "";
             if (!string.IsNullOrEmpty(sql))
-                query = string.Format("SELECT {0} FROM (SELECT ROW_NUMBER() OVER (ORDER BY {1}) AS Row, {0} FROM ({2}) AS PagedTable {3}) AS Paged ", columns, orderBy, sql, where);
+                query = string.Format("SELECT {0} FROM (SELECT ROW_NUMBER() OVER (ORDER BY {1}) AS Row, {0} FROM ({3}) AS PagedTable {4}) AS {2} ", columns, orderBy, TableName, sql, where);
             else
-                query = string.Format("SELECT {0} FROM (SELECT ROW_NUMBER() OVER (ORDER BY {1}) AS Row, {0} FROM {2} {3}) AS Paged ", columns, orderBy, TableName, where);
+                query = string.Format("SELECT {0} FROM (SELECT ROW_NUMBER() OVER (ORDER BY {1}) AS Row, {0} FROM {2} {3}) AS {2} ", columns, orderBy, TableName, where);
 
             var pageStart = (currentPage - 1) * pageSize;
             query += string.Format(" WHERE Row > {0} AND Row <={1}", pageStart, (pageStart + pageSize));
